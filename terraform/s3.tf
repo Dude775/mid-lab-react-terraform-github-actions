e2f4c1522c43db6c1ec2a19dcf1b9e0a46c4f568
David Rubin <idf775@gmail.com>
2026-07-01 01:15:44 +0300

    bucket policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -30,3 +30,38 @@ resource "aws_s3_bucket_ownership_controls" "site" {
     object_ownership = "BucketOwnerEnforced"
   }
 }
+data "aws_iam_policy_document" "site_bucket_policy" {
+  statement {
+    sid = "AllowCloudFrontRead"
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.site.arn}/*"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values = [
+        aws_cloudfront_distribution.site.arn
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site.id
+  policy = data.aws_iam_policy_document.site_bucket_policy.json
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.site
+  ]
+}
