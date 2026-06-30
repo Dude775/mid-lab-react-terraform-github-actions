c097a6b134e23dbbdbe43a57b124804e48cb17fb
David Rubin <idf775@gmail.com>
2026-07-01 01:12:25 +0300

    add s3

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,13 @@ output "project_name" {
   description = "Project name"
   value       = var.project_name
 }
+
+output "s3_bucket_name" {
+  description = "S3 bucket name for the React site"
+  value       = aws_s3_bucket.site.bucket
+}
+
+output "s3_bucket_arn" {
+  description = "S3 bucket ARN"
+  value       = aws_s3_bucket.site.arn
+}
