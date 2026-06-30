fc4c61ab99778c289894b2e6ea635200dbcc65c8
David Rubin <idf775@gmail.com>
2026-07-01 01:14:31 +0300

    add cf

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,60 @@
+locals {
+  s3_origin_id = "${var.project_name}-s3-origin"
+}
+
+resource "aws_cloudfront_origin_access_control" "site" {
+  name                              = "${var.project_name}-oac"
+  description                       = "OAC for React site S3 bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "site" {
+  enabled             = true
+  comment             = "CloudFront distribution for ${var.project_name}"
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id                = local.s3_origin_id
+    origin_access_control_id = aws_cloudfront_origin_access_control.site.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = local.s3_origin_id
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD"]
+
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+  }
+
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = merge(var.common_tags, {
+    Name = "${var.project_name}-cdn"
+  })
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,17 @@ output "s3_bucket_arn" {
   description = "S3 bucket ARN"
   value       = aws_s3_bucket.site.arn
 }
+output "cloudfront_distribution_id" {
+  description = "CloudFront distribution ID"
+  value       = aws_cloudfront_distribution.site.id
+}
+
+output "cloudfront_domain_name" {
+  description = "CloudFront domain name"
+  value       = aws_cloudfront_distribution.site.domain_name
+}
+
+output "website_url" {
+  description = "Website URL through CloudFront"
+  value       = "https://${aws_cloudfront_distribution.site.domain_name}"
+}
